71cc36ad18ab4b5f314958895aac0fc50333d8e0
Tomas18798 <80466134+Tomas18798@users.noreply.github.com>
2021-11-09 21:11:01 -0300

    Reporte

---
+++ Report_Veterinaria/Report_Veterinaria/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Report_Veterinaria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.tipomascotasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new Report_Veterinaria.Reports.DataSet1();
+            this.tipo_mascotasTableAdapter = new Report_Veterinaria.Reports.DataSet1TableAdapters.tipo_mascotasTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.tipomascotasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.tipomascotasBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Report_Veterinaria.Reports.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(3, 4);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(806, 443);
+            this.reportViewer1.TabIndex = 0;
+            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // tipomascotasBindingSource
+            // 
+            this.tipomascotasBindingSource.DataMember = "tipo_mascotas";
+            this.tipomascotasBindingSource.DataSource = this.dataSet1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tipo_mascotasTableAdapter
+            // 
+            this.tipo_mascotasTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 370);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tipomascotasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private Reports.DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource tipomascotasBindingSource;
+        private Reports.DataSet1TableAdapters.tipo_mascotasTableAdapter tipo_mascotasTableAdapter;
+    }
+}
+
